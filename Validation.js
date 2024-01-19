f449307c543262f5fc81977a811f771a25c460a3
Kimutai101 <tiropgarreth@gmail.com>
2024-01-19 21:49:41 +0300

    Created READme.md and Validation.js files

---
+++ Validation.js
@@ -0,0 +1,20 @@
+var nameError = document.getElementById("name-error");
+var phoneError = document.getElementById("phone-error");
+var emailError = document.getElementById("email-error");
+var messageError = document.getElementById("message-error");
+var submitError = document.getElementById("submit-error");
+
+function validationName(){
+    var name = document.getElementById('contact-name').value;
+
+    if(name.length == 0){
+        nameError.innerHTML = 'Name is required';
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = 'Write full Name';
+        return false;
+    }
+    nameError.innerHTML = 'valid';
+    return true;
+}
