0ea0b0451b1c63c457652f4c57b8bbd52386631e
Kimutai101 <tiropgarreth@gmail.com>
2024-01-22 06:36:22 +0300

    Completed this project

---
+++ READme.md
@@ -0,0 +1,3 @@
+**Description**
+- Using Javascript gto validate inputs by user
+- HTML makes the layout of the form,CSS for design and Javascript for validation
--- Validation.js
+++ Validation.js
@@ -4,7 +4,7 @@ var emailError = document.getElementById("email-error");
 var messageError = document.getElementById("message-error");
 var submitError = document.getElementById("submit-error");
 
-function validationName(){
+function validateName(){
     var name = document.getElementById('contact-name').value;
 
     if(name.length == 0){
@@ -15,6 +15,71 @@ function validationName(){
         nameError.innerHTML = 'Write full Name';
         return false;
     }
-    nameError.innerHTML = <i class="fa-solid fa-circle-check"></i>;
+
+    nameError.innerHTML = '<i class = "fas fa-check-circle"></i>';
     return true;
+    
+}
+
+function validatePhone(){
+    var phone = document.getElementById('contact-phone').value;
+
+    if(phone.length == 0){
+        phoneError.innerHTML = 'Phone Number is required';
+        return false;
+    }
+    if(phone.length !== 10){
+        phoneError.innerHTML = 'Phone Number should be 10 digits';
+        return false;
+    }
+    if(!phone.match(/^[0-9]{10}$/)){
+        phoneError.innerHTML = 'Invalid Phone number';
+        return false;
+    }
+
+    phoneError.innerHTML = '<i class="fa-duotone fa-circle-check"></i>'
+    return true;
+}
+
+function validateEmail(){
+    var email = document.getElementById('contact-email').value;
+
+    if(email.length == 0){
+        emailError.innerHTML = 'Email is required';
+        return false;
+    }
+
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = 'Email Invalid'
+        return false
+    }
+
+    emailError.innerHTML = '<i class = "fas fa-check-circle"></i>';
+    return true;
+}
+
+function validateMessage(){
+    var message = document.getElementById('contact-message').value;
+    var required = 40;
+    var left = required - message.length;
+
+    
+    if(left>0){
+        messageError.innerHTML = left + ' more characters required';
+        return false;
+    }
+
+    messageError.innerHTML = '<i class = "fas fa-check-circle"></i>';
+    return true;
+}
+
+function validateForm(){
+    //First, it will check for executions of all the functions we have added.
+
+    if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){
+        submitError.style.display = 'block';
+        submitError.innerHTML = "Please fix error to submit";
+        setTimeout(function(){submitError.style.display = 'none';}, 3000);
+        return false;
+    }
 }
